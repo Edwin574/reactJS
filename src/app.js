db02b52424263ddb132cacac07a28024ef8b3e02
Edwin574 <owinoedwin574@gmail.com>
2022-12-21 02:29:34 +0300

    adding bootstrap and custom styling

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import "./app.css";
+import Products from "./Components/Products";
+
+function App() {
+  return (
+    <div className="row">
+      <div className="col-lg-8 mx-auto">
+        <Products></Products>
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;
