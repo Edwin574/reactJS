08586c2e692633c66edcf492f4c92bad65ee3196
Edwin574 <owinoedwin574@gmail.com>
2022-12-27 16:10:32 +0300

    using props

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,37 +1,29 @@
-
-
 let productCount = 0;
-let ImageUrl = require("../images/Standards.png");
-let isAvailable = "available";
-let badgeClass = "badge-margin-left-100 badge";
-
-if (isAvailable == "Available") {
-  badgeClass = badgeClass + " bg-success";
-} else {
-  badgeClass = badgeClass + " bg-danger";
-}
 
 function displayFormattedProductCount() {
   return productCount > 0 ? productCount : "zero";
 }
 
-function Products() {
+function Products(props) {
+  let badgeClass = "badge-margin-left-100 badge";
+
+  badgeClass += props.isAvailable ? " bg-success" : " bg-danger";
+
   return (
     <ul className="list-group shadow">
       <li className="list-group-item">
         <div className="media align-items-lg-center flex-column flex-lg-row p-3 d-flex">
           <div className="media-body order-2 order-lg-1">
-            <h5 className="mt-0 font-weight-bold mb-2">Fresh Milk</h5>
+            <h5 className="mt-0 font-weight-bold mb-2">{props.name}</h5>
             <p className="font-italic text-muted mb-0 small">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit
-              fuga autem maiores necessitatibus.
+              {props.description}
             </p>
             <div className="d-flex align-items-center justify-content-start mt-1">
               <h6
                 className="font-weight-bold my-2"
                 style={{ "margin-right": 30 }}
               >
-                $120.00
+                {props.price}
               </h6>
               <button className="btn btn-primary">-</button>
               <span style={{ padding: "0px 14px", "font-size": 12 }}>
@@ -40,11 +32,13 @@ function Products() {
               <button className="btn btn-primary">+</button>
               {/* <span className={badgeClass}>{isAvailable}>
                 Available</span> */}
-              <span className={badgeClass}>{isAvailable}</span>
+              <span className={badgeClass}>
+                {props.isAvailable ? "Available" : "unavailable"}
+              </span>
             </div>
           </div>
           <img
-            src={ImageUrl}
+            src={props.imageUrl}
             alt="Generic placeholder image"
             width="200"
             className="ml-lg-5 order-1 order-lg-2"
