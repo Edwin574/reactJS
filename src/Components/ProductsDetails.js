4d4d1c55b8766a0a457b8758298dd5c96c5a2b69
Edwin574 <owinoedwin574@gmail.com>
2022-12-28 00:31:55 +0300

    use state hooks

--- src/Components/ProductsDetails.js
+++ src/Components/ProductsDetails.js
@@ -1,12 +1,6 @@
 import React from "react";
-
+import { useState } from "react";
 import Button from "./Button";
-
-let productCount = 0;
-
-function displayFormattedProductCount() {
-  return productCount > 0 ? productCount : "zero";
-}
 
 // function mouseOver() {
 //     console.log('mouse over event happened')
@@ -14,20 +8,34 @@ function displayFormattedProductCount() {
 
 function ProductDetails(props) {
   let badgeClass = "badge-margin-left-100 badge";
-
   badgeClass += props.isAvailable ? " bg-success" : " bg-danger";
+    // let productCount = 0;
+    let [productCount,updateCount]=useState(0)
+
+  function displayFormattedProductCount() {
+    return productCount > 0 ? productCount : "zero";
+  }
+
+  let incrementProductCount = function () {
+    // productCount++;
+    // console.log(productCount);
+      updateCount(++productCount)
+  };
+  let decrementProductCount = function () {
+    // productCount--;
+    // console.log(productCount);
+    updateCount(--productCount)
+  };
   return (
     <div className="d-flex align-items-center justify-content-start mt-1">
       <h6 className="font-weight-bold my-2" style={{ marginRight: 30 }}>
         {props.price}
       </h6>
-      <Button>-</Button>
+      <Button eventHandler={decrementProductCount}>-</Button>
       <span style={{ padding: "0px 14px", fontSize: 12 }}>
         {displayFormattedProductCount()}
       </span>
-      <Button>+</Button>
-      {/* <span className={badgeClass}>{isAvailable}>
-      Available</span> */}
+      <Button eventHandler={incrementProductCount}>+</Button>
       <span className={badgeClass}>
         {props.isAvailable ? "Available" : "unavailable"}
       </span>
